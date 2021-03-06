a3115b56f916323011424e2c6e3ab2ee712e71a9
gilliancarballo <gillian.carballo0319@gmail.com>
2021-03-06 17:54:46 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'users');
+    define('DB_NAME', 'users');
+
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if ($link === false) {
+        die('Error: Could not connect . ' . mysqli_connect_error());
+    }
+
